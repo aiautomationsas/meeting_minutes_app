f2299388e54817aedf3744185c6194a8e92003d3
Santiago Sanchez <visanto.agencia@gmail.com>
2024-10-01 14:41:20 -0500

    fix app

--- app/app.py
+++ app/app.py
@@ -1,37 +1,37 @@
-import requests
+import asyncio
 
-url = "https://ht-rash-variability-38-164af1dbe40454e9bd46e5f19b24bc7c.default.us.langgraph.app/runs/stream"
+from langgraph_sdk import get_client
 
-payload = {
- "assistant_id": "a2c2bca2-93da-446c-bdb5-d292750bc4fb",
-    "input": {
+url = "https://ht-rash-variability-38-164af1dbe40454e9bd46e5f19b24bc7c.default.us.langgraph.app"
+client = get_client(url=url)
+assistant_id = "agent"
+
+async def main():
+    # Create thread
+    thread = await client.threads.create()
+    print(f"Created thread: {thread}")
+
+    input_data = {
         "audioFile": "",
-        "transcript": "Crea una acta de reunión ficticia sobre la creación de un nuevo producto de software para una empresa de tecnología. No tengo la transcripción.",
-        "wordCount": 100,
+        "transcript": "Crea un acta ficticia de na reunión de desarrollo de software. No tengo la transcripción",
+        "wordCount": 500,
         "critique": "",
         "approved": False,
         "minutes": "",
         "outputFormatMeeting": "",
         "messages": []
-    },
-    "metadata": {},
-    "config": {
-        "tags": [],
-        "configurable": {}
-    },
-    "interrupt_before": ["human_critique"],
-    "stream_mode": ["values"]
-}
-headers = {"Content-Type": "application/json",
-           "X-Api-Key": "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"}
+    }
 
-response = requests.post(url, json=payload, headers=headers)
+    async for chunk in client.runs.stream(
+        thread['thread_id'],
+        assistant_id,
+        input=input_data,
+        interrupt_before=["human_critique"],
+        stream_mode="values"
+    ):
+        print(f"Receiving new event of type: {chunk.event}...")
+        print(chunk.data)
+        print("\n")
 
-if response.status_code == 200:
-    for line in response.iter_lines():
-        if line:
-            decoded_line = line.decode('utf-8')
-            print(decoded_line)
-else:
-    print(f"Error en la solicitud. Código de estado: {response.status_code}")
-    print("Contenido de la respuesta:", response.text)
+if __name__ == "__main__":
+    asyncio.run(main())
